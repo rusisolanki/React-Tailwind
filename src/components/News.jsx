045af5fb4d2c44da9fb85e164a5ab85af148719e
Rusi Solanki <rusismac@Rusis-MacBook-Pro.local>
2023-09-17 21:13:08 -0400

    Intial Commit

---
+++ src/components/News.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div className='w-full py-16 text-white px-4'>
+        <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
+            <div className='lg:col-span-2 lg:py-4'>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want tips and tricks to optimize your flow?</h1>
+                <p>Sign up to our newsletter</p>
+            </div>
+            <div className='my-4'>
+                <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+                    <input className='p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter your Email' />
+                    <button className='bg-[#00df9a] w-[200px] rounded-full font-medium ml-4 my-6 px-6 py-3 text-black'>Notify me</button>
+                </div>
+                <p className='md:text-left sm:text-center'>Read our <span className='text-[#00df9a]'>Privacy Policy</span></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default News
